4c4c2781ff352c05f72ba7ab96e1706478f6d960
Leonard Herold <35725355+l8518@users.noreply.github.com>
2019-10-09 17:29:09 +0200

    Add tmp write - cnt with scala

--- notebooks/transform_and_index.py
+++ notebooks/transform_and_index.py
@@ -1,9 +1,9 @@
 # Databricks notebook source
-OUTPUT_DIR = "/mnt/group07/pipeline"
+OUTPUT_DIR = "/mnt/group07/classification_test_files/classification_test"
 
 # COMMAND ----------
 
-tags_df = spark.read.parquet("dummy_test.parquet")
+tags_df = spark.read.parquet("test_sorted_classification.parquet")
 flickr_df = spark.read.parquet("/mnt/group07/full/flickr.parquet")
 
 # COMMAND ----------
@@ -12,40 +12,38 @@ tags_df = tags_df.orderBy(["image_id"])
 
 # COMMAND ----------
 
-import os
+from pyspark.sql.functions import desc, asc
  
 tags_wmeta_df = tags_df.join(flickr_df.select("id", "photo_video_page_url", "title"), tags_df.image_id == flickr_df.id).drop("id")
-tags_wmeta_df = tags_wmeta_df.orderBy(["tag", "certainty", "image_id"])
+tags_wmeta_df = tags_wmeta_df.orderBy([asc("label"), desc("confidence"), asc("image_id")])
 
 # COMMAND ----------
 
-from pyspark.sql.functions import col
-
-tags_tag_cnt_df = tags_wmeta_df.groupBy("tag").count().orderBy("count", ascending=False)
-tags_tag_cnt_df = tags_tag_cnt_df.filter(col("count") > 20)
+tags_wmeta_df.write.mode("overwrite").partitionBy('label').json(os.path.join(OUTPUT_DIR, "tmp", "tags"))
+# continue with scala - i suppose
 
 # COMMAND ----------
 
-from pyspark.sql import functions as F
-
-total = tags_tag_cnt_df.groupBy().agg(F.sum("count"))
-total.show()
+import os
+#tags_wmeta_df.write.mode("overwrite").option("maxRecordsPerFile", 100).partitionBy('label').json(os.path.join(OUTPUT_DIR, "subset"))
 
 # COMMAND ----------
 
-tags_subset_df = tags_wmeta_df.join(tags_tag_cnt_df, tags_tag_cnt_df.tag == tags_wmeta_df.tag).drop(tags_tag_cnt_df.tag).drop("count")
+from  pyspark.sql.functions import input_file_name
 
+#df_chucks= spark.read.format("json").load("/mnt/group07/classification_test_files/classification_test")
+#df_chucks = df_chucks.withColumn("C_Location",input_file_name())
 
 # COMMAND ----------
 
-tags_subset_df.write.mode("overwrite").option("maxRecordsPerFile", 100).partitionBy('tag').json(os.path.join(OUTPUT_DIR, "subset"))
+#df_chucks.show(truncate=False)
 
 # COMMAND ----------
 
 from pyspark.sql import functions as F
 # check: create folder for each tag
 # check: write (firt approach) one big json for everything
-# ceck: write chunked version
+# check: write chunked version
 #       -> chunk big json for each tag into serveral small ones
 # TODO: write inv list for all tags that were written (determine name of the chunks? ; )
 # --> create df which maps tag to chunckname -> transform that to a json-baed inv list
