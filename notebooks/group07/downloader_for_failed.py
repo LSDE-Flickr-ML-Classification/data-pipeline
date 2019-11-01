cc79b4509a8288c88399c29611c9ec8ab9a0f32d
Leonard Herold <35725355+l8518@users.noreply.github.com>
2019-11-01 22:46:16 +0100

    Update: .gitignore LICENSE README.md notebooks

---
+++ notebooks/group07/downloader_for_failed.py
@@ -0,0 +1,173 @@
+# Databricks notebook source
+import requests
+import io
+import os
+from pyspark.sql.functions import col
+from timeit import default_timer as timer
+from datetime import timedelta
+from torchvision import transforms
+from PIL import Image
+
+# COMMAND ----------
+
+# OUTPUT_FILE = "/mnt/group07/final_data_product/downloaded_images-1.parquet"
+OUTPUT_FILE = "/mnt/group07/final_data_product/downloaded_images-retry-final_v2.parquet"
+# INPUT_FILE = "/mnt/group07/final_data_product/classification_input.parquet"
+INPUT_FILE = "/mnt/group07/final_data_product/download_input-retry.parquet"
+
+# COMMAND ----------
+
+class ImageDownloader:
+    transform_function = transforms.Compose([
+        transforms.Resize(224),
+        transforms.CenterCrop(224)
+    ])
+
+    @staticmethod
+    def transform_image(image_bytes):
+        image = Image.open(image_bytes)
+        transformed_image = ImageDownloader.transform_function(image)
+        return transformed_image
+    
+
+# COMMAND ----------
+
+def download_and_save_image(row):
+  image_bytes = None
+  image_height = None
+  image_width = None
+  image_bytes_size = None
+  
+  if row.longitude is not None:
+    try:
+      img_s3_path = os.path.join("/dbfs/mnt/group01/images", "{0}_{1}_{2}.{3}".format(row.id, row.longitude, row.latitude, row.photo_video_extension_original) )
+      s3_img_file = open(img_s3_path, "rb")
+      img_read = s3_img_file.read()
+      s3_img_file.close()
+      # get image stats:
+      image_bytes = bytearray(img_read)
+
+      # resize image
+      image_bytes_size = len(img_read)
+      image = Image.open(io.BytesIO(image_bytes))
+      image_height = image.size[1]
+      image_width = image.size[0]
+      
+      img = ImageDownloader.transform_image(io.BytesIO(image_bytes))
+      buffer = io.BytesIO()
+      # Convert to RGB to fix error (POST FIX)
+      img = img.convert('RGB')
+      img.save(buffer, format='PNG')
+      image_bytes = bytearray(buffer.getvalue())
+      
+      return (row.id, image_bytes, image_bytes_size, image_height, image_width, "s3")
+    except Exception as e:
+      # do nothing, and retry on real flickr data
+      pass
+  
+  # try to download:
+  try:
+    req = requests.get(row.photo_video_download_url)
+    if req.status_code == 200:
+      image_bytes = bytearray(req.content)
+      image_bytes_size = len(req.content)
+      # resize image
+      image = Image.open(io.BytesIO(image_bytes))
+      image_height = image.size[1]
+      image_width = image.size[0]
+      
+      img = ImageDownloader.transform_image(io.BytesIO(image_bytes))
+      buffer = io.BytesIO()
+      # Convert to RGB to fix error (POST FIX)
+      img = img.convert('RGB')
+      img.save(buffer, format='PNG')
+      image_bytes = bytearray(buffer.getvalue())
+
+    else:
+      image_bytes = None
+    return (row.id, image_bytes, image_bytes_size, image_height, image_width, req.status_code)
+  except Exception as e:
+    s = str(e)
+    return (row.id, None, None, None, None, s)
+
+# COMMAND ----------
+
+# Start Timing
+start_download = timer()
+
+# COMMAND ----------
+
+# DBTITLE 1,Whole Download Pipeline
+from pyspark.sql.types import StructField, BinaryType, StructType, StringType, IntegerType
+
+# Define Schema for output table
+schema = StructType(fields=[
+    StructField('id', StringType(), True),
+    StructField('img_binary', BinaryType(), True),
+    StructField('img_size', IntegerType(), True),  
+    StructField('img_width', IntegerType(), True),
+    StructField('img_height', IntegerType(), True),
+    StructField('error_code', StringType(), True),
+  ])
+
+
+df_dl_links = spark.read.parquet(INPUT_FILE)
+
+print("About to process {0} rows".format(df_dl_links.count()))
+# Sort the dataframe along id
+df_dl_links = df_dl_links.sort("id")
+# Load the previously downloaded images (if job restarted)
+if os.path.isdir("/dbfs" + OUTPUT_FILE):
+  download_history_df = spark.read.parquet(OUTPUT_FILE)
+else:
+  download_history_df = spark.createDataFrame([], schema=schema)
+
+# Determine the rows that have been downloaded already + determine the rows to be downloaded again
+already_downloaded_ids = download_history_df.where(col("img_binary").isNotNull()).select("id")
+to_download_df = df_dl_links.join(already_downloaded_ids, 'id', 'left_anti')
+already_downloaded = already_downloaded_ids.count()
+if (already_downloaded > 0):
+  print("Restarted from {0} rows - downloading {1} rows".format(already_downloaded, to_download_df.count()))
+
+# COMMAND ----------
+
+batch_map = to_download_df.rdd.map(download_and_save_image)
+final_df = spark.createDataFrame(batch_map, schema=schema)
+
+# COMMAND ----------
+
+# Create the new download file from previous downloads + new downloads
+download_history_df = download_history_df.where(col("img_binary").isNotNull()).union(final_df)
+
+# Write results to storage
+download_history_df.write.format("parquet").save(OUTPUT_FILE)
+
+# COMMAND ----------
+
+# Finish timer
+end_download = timer()
+download_in_seconds = timedelta(seconds=end_download-start_download).total_seconds()
+print(download_in_seconds)
+
+# COMMAND ----------
+
+df_output = spark.read.parquet(OUTPUT_FILE)
+
+# COMMAND ----------
+
+df_output.groupBy("error_code").count().show(truncate=False)
+
+# COMMAND ----------
+
+df_output.groupBy("error_code").count().show(truncate=False)
+
+# COMMAND ----------
+
+df_output.count()
+
+# COMMAND ----------
+
+df_output.show()
+
+# COMMAND ----------
+
